dd88aa73f149247a2a5b5abde3a69de3855e0f54
Luke-Lethborg <166884599+Luke-Lethborg@users.noreply.github.com>
2025-05-20 19:45:05 +1000

    Add files via upload
    ** Below is a little wordy for an initial upload **
    
    https://www.youtube.com/watch?v=AhAxLiGC7Pc
    ASP.NET Core Full Course For Beginners
    Julio Casal
    https://www.youtube.com/@juliocasal
    22/02/2024
    3:43:17
    This is code to 1:20:22 of 3:43:17.
    The next section we will refactor some code like Program.cs
    
    Purpose of this course is to:
    #01	Create ASP.Net Core Apps
    #02	Understand REST APIs
    #03	Implement CRUD Endpoints
    #04	Data Transfer Objects (DTOs)
    #05	Extension Methods
    #06	Route Groups
    #07	Handle Invalid Inputs
    #08	Entity Framework Core
    #09	Configuration System
    #10	Dependency Injection
    #11	Service Lifetime
    #12	Mapping Entities to DTOs
    #13	Asynchronous Programming
    #14	Frontend Integration
    
    To use this code:
    Open in Visual Studio Code at File Location at 'Program02'
    On Terminal
    By default your at ....\Program02
    CD GameStore.api
    Now at ....\Program02\GameStore.api
    dotnet run
    Open File: games.http
    Above each command like: GET http://localhost:5250/games
    Select: Send Request
    
    You need Extensions like:
    Live Server by Ritwick Dey and REST Client by Huachao Mao
    
    So far we have a basic CRUD happening
    
    "This gets all"
    GET http://localhost:5250/games
    
    "This gets id of 1"
    GET http://localhost:5250/games/1
    
    "This create a new entry, making it's own ID"
    POST http://localhost:5250/games
    Content-Type: application/json
    
    {
        "name":        "Minecraft",
        "genre":       "Kids and Family",
        "price":       19.99,
        "releaseDate": "2011-11-18"
    }
    
    "This updates an existing entry"
    PUT http://localhost:5250/games/1
    Content-Type: application/json
    
    {
        "name":        "Street Fighter II Turbo",
        "genre":       "Fighting",
        "price":       9.99,
        "releaseDate": "1991-02-01"
    }
    
    "This deletes an existing entry"
    DELETE http://localhost:5250/games/4
    
    There is more work to be done, but the first 1:20:22

---
+++ GameStore.api/Program.cs
@@ -0,0 +1,98 @@
+using GameStore.api.Dtos;
+
+/*
+ * https://www.youtube.com/watch?v=AhAxLiGC7Pc
+ * Julio Casal      https://www.youtube.com/@juliocasal
+ * ASP.NET Core Full Course For Beginners
+ * 22/02/2024 | 3:43:17
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+const string GetGameEndpointName = "GetGame";
+
+List<GameDto> games = [ 
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)),
+     new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27))
+];
+
+//Get /games (All Games)
+app.MapGet("games", () => games);
+
+//Get /Games/1 (That is get game with specificed ID)
+//app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+//.WithName(GetGameEndpointName);
+
+//Get /Games/1 (That is get game with specificed ID)
+app.MapGet("games/{id}", (int id) => 
+{
+    GameDto? game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
+
+//POST /games (Create a new entry)
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate);
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+});
+
+//Put Games (At next index) (Does not prevent duplicates)
+app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    if(index == -1)
+    {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updateGame.Name,
+        updateGame.Genre,
+        updateGame.Price,
+        updateGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /Games/1 (At index specfied)
+app.MapDelete("games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
+});
+
+//app.MapGet("/", () => "Hello World!! - This is [ASP.NET Core Full Course For Beginners] by [Julio Casal] at [3:43:17] - Luke is Great!");
+
+app.Run();
